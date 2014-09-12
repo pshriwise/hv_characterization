6afeda9b26f8ab0e355b0d8e176e83716e3f2d94
pshriwise <shriwise@wisc.edu>
2014-09-12 15:16:05 -0500

    Created a new program file for constructing a controlled, high-valence mesh.

---
+++ create_hv_mesh.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <sstream>
+#include <iomanip> // for setprecision
+#include <limits> // for min/max values
+#include <assert.h>
+#include <math.h>
+#include <time.h>
+#include <vector>
+
+#include "MBCore.hpp"
+
+
+
+
+int main(int argc, char **argv)
+{
+
+//create a moab interface
+MBInterface *mbi = new MBCore;
+
+
+
+return 0;
+
+}
