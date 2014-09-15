7683f495b1ff9ebfcfe691cb70ffec9c4c403e8a
pshriwise <shriwise@wisc.edu>
2014-09-15 14:20:04 -0500

    Finished code for selecting the surface for modification.

--- create_hv_mesh.cpp
+++ create_hv_mesh.cpp
@@ -30,20 +30,51 @@ int main(int argc, char **argv)
   
   std::cout << "There are " << surfs.size() << " surfaces in this model." << std::endl;
 
+moab::EntityHandle hv_surf;
+
   //Now to find one of the surfaces that is constant in z (for convenience)
-  for( MEntVector::iterator i = surfs.begin(); i != surfs.end() ; i++)
+for( unsigned int i = 0 ; i < surfs.size() ; i++)
     {
       //get the meshset handle for this surface
-      moab::EntityHandle sh = surfs[0]->mesh_handle();
+      moab::EntityHandle sh = surfs[i]->mesh_handle();
+
       //get the triangles in this meshset
       std::vector<moab::EntityHandle> tris;
       mk->moab_instance()->get_entities_by_type( sh, MBTRI, tris);
       std::cout << "There are " << tris.size() << " triangles in this surface." << std::endl;
 
 
+
+//setup a check vector
+MBCartVect check;
+check [0] = 0; check [1] = 0; check[2] = 1;
+//get the normal for the first triangle on each surface
+
+//get the verts for each triangle and their coordinates
+std::vector<moab::EntityHandle> verts;
+mk->moab_instance()->get_connectivity( &(tris[0]), 1, verts);
+
+MBCartVect coords[3];
+
+mk->moab_instance()->get_coords( &(verts[0]), 3, coords[0].array() );
+
+MBCartVect tri_norm;
+gen::triangle_normal( coords[0], coords[1], coords[2], tri_norm);
+
+std::cout << "The normal vector for this surface of the cube is " << tri_norm << std::endl;
+      
+
+if( tri_norm == check){
+hv_surf = sh;
+break;
+}
+     
     }
   
   
   return 0;
 
 }
+
+
+
