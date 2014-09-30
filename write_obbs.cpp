ca6d76b7e1717ee1d4cfd86c604b2d18de15b643
pshriwise <shriwise@wisc.edu>
2014-09-30 16:05:41 -0500

    Changed the write_obb function to take in any dagmc-ready .h5m file.

--- write_obbs.cpp
+++ write_obbs.cpp
@@ -5,33 +5,24 @@
 
 int main( int argc, char** argv) 
 {
-
   //hangle arguments
-  if( 3 != argc )
+  if( 2 != argc )
     {
-      std::cout << "Please include all necessary arguments! Exiting..." << std::endl;
-      std::cout << "To generate a high valence region with area ";
-      std::cout << "fraction A_f (double) and verts of valency n:" << std::endl;
-      std::cout << "$ ./create_hv_mesh <A_f> <n>" << std::endl;
+      std::cout << "To write the oriented bounding boxes of a dagmc .h5m file";
+      std::cout << "in a .vtk format:"<< std::endl;
+      std::cout << "$./write_obbs <filename.h5m>" << std::endl;
       return 0;
     } 
 
 
-  double A_f  = atof(argv[1]);
-
-  int valence = atoi(argv[2]);
-
-  //make sure A_f is valid
-  if ( (1.0/6.0) < A_f || A_f < 0 ) { std::cout << "Area fraction A_f must be between 0 and 1/6 (for now.)" << std::endl; return 0; }
-
-  //now create the hv mesh and write to file 
-  prep_mesh( A_f, valence );
+  //assign the filename
+  char *filename = argv[1];
 
   //start a dagmc instance and load the modified cube file
   moab::DagMC *dag_inst = moab::DagMC::instance();
 
   moab::ErrorCode result;
-  result = dag_inst->load_file( "cube_mod.h5m" );
+  result = dag_inst->load_file( filename );
   if( MB_SUCCESS != result) return MB_FAILURE;
   
   //generate the obb tree
@@ -39,7 +30,7 @@ int main( int argc, char** argv)
   if( MB_SUCCESS != result) return MB_FAILURE;
   
   moab::Range vols;
-  result = get_volumes( dag_inst->moab_instance(), vols ); 
+  result = get_volumes( dag_inst->moab_instance(), vols );
 
   
   //now write the OBBs to a set of files based on depth in tree
