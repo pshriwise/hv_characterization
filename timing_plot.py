0dbc0f4079f2c499a8258e6a3d9d6629042362aa
pshriwise <shriwise@wisc.edu>
2015-09-24 09:53:28 -0500

    Added an option for plotting on log or linear scale.

--- timing_plot.py
+++ timing_plot.py
@@ -1,6 +1,7 @@
 
 import sys
 from matplotlib import pyplot as plt
+from matplotlib import colors
 import numpy as np
 import argparse as ap
 
@@ -10,11 +11,11 @@ parser.add_argument('--datafile', type=str, default='data.dat', help='Datafile c
 parser.add_argument('--title',dest='plt_title', type=str,
                     default='High Valance Characterizaion Timing Plot',
                     help = 'Used to set the title of the created plot')
+parser.add_argument('--no-log',dest='no_log',action='store_true',default=False,help='Indicates that the colorbar should not be in log scale.')
 
 args = parser.parse_args(sys.argv[1:])
 
-
-#Retrieve data
+#Retrieve data from specified datafile
 raw = np.fromfile(args.datafile,dtype=float,count=-1,sep="\t")
 raw = raw.reshape((raw.shape[0]/4,4))
 
@@ -36,8 +37,15 @@ ax.set_xlabel("Valance")
 ax.set_ylabel("Area Fraction")
 
 plt.title(args.plt_title)
-plt.pcolormesh(Xv,Yv,Z,)
-plt.colorbar(format = '%2.0E')
+
+if args.no_log:
+    mesh = plt.pcolormesh(Xv,Yv,Z)
+else:
+    mesh = plt.pcolormesh(Xv,Yv,Z,norm=colors.LogNorm())
+
+mesh.set_clim(1e-6,1e-03)
+cb = plt.colorbar(format = '%2.0E')
+cb.set_label("Time (s)")
 plt.show()
 
 
