4b67b7c6d7536a5b3d6938f8dd31fcee66e9f319
pshriwise <shriwise@wisc.edu>
2016-06-07 16:26:17 -0500

    Adding program options to read in user input.

--- single_ray_traversal/traverse_ray.cpp
+++ single_ray_traversal/traverse_ray.cpp
@@ -3,6 +3,7 @@
 #include "moab/Types.hpp"
 #include "moab/Core.hpp"
 #include "moab/Interface.hpp"
+#include "moab/ProgOptions.hpp"
 #include "ray_writer.hpp"
 
 
@@ -10,6 +11,14 @@ int main (int argc, char** argv)
 {
 
   moab::Interface *MBI = new moab::Core();
+
+  ProgOptions po("Traverse Ray: A program for writing out all OBBs encoutnered by a single ray traversal.");
+
+  int vol_id;
+  double ray_origin[3];
+  po.addRequiredArg<int>("vol_id", "GLOBAL ID of the volume to fire the ray in.", &vol_id);
+  //  po.addRequiredArg<double[3]>("Ray Origin", "Origin of the ray to fire.", &ray_origin);
+  
 
   
   return 0;
